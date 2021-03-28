d26cabb266f935be3a3597c85d5c2ef8af49c1eb
Changdae <einsvision@gmail.com>
2021-03-28 23:27:58 +0900

    awesome chat engine

--- teams/src/App.js
+++ teams/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h3>Hello Teams</h3>
+      <ChatEngine
+        height='100vh'
+        userName='changdae'
+        userSecret='123123'
+        projectID='e1a09eda-981e-48d2-86e6-2ee6a42ff771'
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
     </div>
   );
 }
