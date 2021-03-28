bd6bcee771706f58e2a35800030bf79af7b3d321
Changdae <einsvision@gmail.com>
2021-03-28 23:59:31 +0900

    chat messages components

--- teams/src/components/ChatFeed.js
+++ teams/src/components/ChatFeed.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
 
 function ChatFeed(props) {
   console.log(props)
   return (
     <div>
       ChatFeed
+      <MessageForm />
+      <MyMessage />
+      <TheirMessage />
     </div>
   )
 }
