003322c193038c4f3d7b4201a1750232fb8a3bf1
Changdae <einsvision@gmail.com>
2021-03-30 07:44:54 +0900

    building messages window

--- teams/src/components/MyMessage.js
+++ teams/src/components/MyMessage.js
@@ -14,7 +14,7 @@ function MyMessage({message}) {
 
   return (
     <div className='message' style={{float: 'right', marginRight: '18px', color:'white', backgroundColor: '#382A50' }}>
-      MyMessage
+      {message.text}
     </div>
   )
 }
